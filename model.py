a16e2330483458db98c6775e111ad815fa99c9ef
raphey <16931750+raphey@users.noreply.github.com>
2020-05-30 22:07:46 -0700

    model training

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
-import tensorflow as tf
+from datetime import datetime
 
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
 
 from data import get_train_and_test_data_plus_raw_test_data
 
@@ -26,9 +26,17 @@ def main():
     (train_features, train_labels), (test_features, test_labels), _ = get_train_and_test_data_plus_raw_test_data()
     model = build_model(train_features.shape[1])
     model.summary()
-    example_batch = train_features[:10]
-    example_result = model.predict(example_batch)
-    print(example_result)
-
+    print('Training model')
+    _ = model.fit(train_features,
+                  train_labels,
+                  epochs=20,
+                  validation_split=0.1,
+                  verbose=1,)
+    print('Evaluating model')
+    model.evaluate(test_features, test_labels)
+    date_string = datetime.today().strftime('%Y%m%d-%H%M%S')
+    model_save_path = 'tm_model_{}.h5'.format(date_string)
+    print('Saving model to {}'.format(model_save_path))
+    model.save(model_save_path)
 
 main()
